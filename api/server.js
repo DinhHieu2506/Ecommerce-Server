3c4075a71d040457fdb9440444ea9a172233e28c
DinhHieu2506 <hieucr78@gmail.com>
2025-07-23 11:12:04 +0700

    update: server

--- api/server.js
+++ api/server.js
@@ -5,7 +5,13 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 const cors = require('cors');
-server.use(cors()); // Cho phép mọi domain, có thể custom nếu cần
+server.use(cors({
+  origin: ['https://ecommerce-dashboard-seven-phi.vercel.app'], // Cho phép mọi domain, có thể custom nếu cần
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Range', 'X-Total-Count'],
+  credentials: true,
+})); // Cho phép mọi domain, có thể custom nếu cần
 
 server.use(middlewares)
 server.use(router)
